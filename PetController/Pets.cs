ecb6bf70e843dcd1897895a74c7c281c4d0194b5
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-02-04 19:05:42 +0400

    Changes to be committed:  	modified:   PetController/Pets.cs  	modified:   PetController/Program.cs

--- PetController/Pets.cs
+++ PetController/Pets.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace PetController
 {
+    [JsonPolymorphic]
+    [JsonDerivedType(typeof(Dog))]
+    [JsonDerivedType(typeof(Cat))]
+    [JsonDerivedType(typeof(Bird))]
+
     public abstract class Pet
     {
         private static int _nextId = 1;
@@ -49,19 +58,16 @@ namespace PetController
         public override void MakeNoise() => Console.WriteLine($"{Name} says Tweet TWEET!");
         public override void PetMove() => Console.WriteLine("Fly");
     }
-
-    public interface IPetDataHandler
-    {
-        void SavePets(List<Pet> pets);
-        List<Pet> LoadPets();
-    }
-
-
     public class PetManager
     {
+        private readonly IPetDataHandler dataHandler;
+        public PetManager(IPetDataHandler Handler)
+        {
 
+            dataHandler = Handler;
+        }
         private List<Pet> PetList { get; set; } = new List<Pet>();
-        
+
         public void AddPet(Pet pet)
         {
             PetList.Add(pet);
@@ -75,6 +81,101 @@ namespace PetController
                 pet.PetMove();
             }
         }
+
+        public void SavePets() => dataHandler.SavePets(PetList);
+        public void LoadPets()
+        {
+            PetList = dataHandler.LoadPets();
+        }
+            
+
+    }
+    public interface IPetDataHandler
+    {
+       void SavePets(List<Pet> pets);
+       List<Pet> LoadPets();
+    }
+
+    public class FilePetDataHandler : IPetDataHandler
+    {
+
+            private readonly string filePath = "pets.json";
+
+        public void SavePets(List<Pet> pets)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                TypeInfoResolver = new DefaultJsonTypeInfoResolver
+                {
+                    Modifiers = { (typeInfo) =>
+            {
+                if (typeInfo.Type == typeof(Pet))
+                {
+                    typeInfo.PolymorphismOptions = new JsonPolymorphismOptions
+                    {
+                        TypeDiscriminatorPropertyName = "$type",
+                        DerivedTypes =
+                        {
+                            new JsonDerivedType(typeof(Dog), "Dog"),
+                            new JsonDerivedType(typeof(Cat), "Cat"),
+                            new JsonDerivedType(typeof(Bird), "Bird")
+                        }
+                    };
+                }
+            }}
+                }
+            };
+
+            string json = JsonSerializer.Serialize(pets, options);
+            File.WriteAllText(filePath, json);
+            Console.WriteLine("Pets Have been saved");
+        }
+        public List<Pet> LoadPets() {
+                if (!File.Exists(filePath)) { 
+                    Console.WriteLine("No Saved Found");
+                    return new List<Pet>();
+                }
+                string json = File.ReadAllText(filePath);
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true,
+                 IncludeFields = true,
+                TypeInfoResolver = new DefaultJsonTypeInfoResolver
+                {
+                    Modifiers = { (typeInfo) =>
+                    { if(typeInfo.Type == typeof(Pet))
+                        {
+                            typeInfo.PolymorphismOptions = new JsonPolymorphismOptions
+                            {
+                                TypeDiscriminatorPropertyName = "$type",
+                                IgnoreUnrecognizedTypeDiscriminators = true,
+                                DerivedTypes =
+                                {
+
+                                    new JsonDerivedType(typeof(Dog), "Dog"),
+                                    new JsonDerivedType(typeof(Cat), "Cat"),
+                                    new JsonDerivedType(typeof(Bird), "Bird")
+
+                                }
+                            };
+                        }
+                    }
+
+                }
+                }
+
+            };
+            try
+            {
+                List<Pet>? pets = JsonSerializer.Deserialize<List<Pet>>(json,options);
+                Console.WriteLine("Pets loaded successfully");
+                return pets ?? new List<Pet>();
+            }
+            catch (Exception ex) { Console.WriteLine($"Error Loading pets:{ex.Message}"); return new List<Pet>(); }
+            }
 
 
     }
--- PetController/Program.cs
+++ PetController/Program.cs
@@ -7,14 +7,17 @@ namespace PetController
     {
         static void Main(string[] args)
         {
-
-            PetManager petManager = new PetManager();
+            IPetDataHandler fileHandler = new FilePetDataHandler();
+            PetManager petManager = new PetManager(fileHandler);
             //we never neaded keyword required
             petManager.AddPet(new Dog("Lord Farkwad", 28));
             petManager.AddPet(new Dog("Lord ", 18));
             petManager.AddPet(new Dog("Farkwad", 14));
             //push
+            petManager.SavePets();
+            petManager.LoadPets();
             petManager.ShowPets();
+
 
         }
     }
