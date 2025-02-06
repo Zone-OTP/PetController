7256bc5e45b65478bd72bdb1b39f99bbfaa342f6
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-02-06 20:15:56 +0400

    Changes to be committed:  	deleted:    PetController/PetController.csproj  	deleted:    PetController/Pets.cs  	modified:   PetController/Program.cs
      Untracked files:
     	PetController/PetController/
     	PetController/PetControllerProgram.csproj

--- PetController/Program.cs
+++ PetController/Program.cs
@@ -12,20 +12,12 @@ namespace PetController
             IPetDataHandlerMemory memoryFileHandler = new InMemoryPetDataHandler();
 
             PetManager petManager = new PetManager(jsonFileHandler, memoryFileHandler);
-            //we never neaded keyword required
             petManager.AddPet(new Dog("Lord Farkwad", 28));
-            petManager.AddPet(new Dog("Lord ", 18));
+            petManager.AddPet(new Dog(" ", 18));
             petManager.AddPet(new Dog("Farkwad", 14));
-<<<<<<< HEAD
-            //commit
-=======
-            //push
             petManager.MemoryOrFile();
->>>>>>> 2160f03dd00330a516960207ba40b8aff2ce8ffc
             petManager.ShowPets();
             petManager.SpesificPet(1);
-
-
         }
 
     }
