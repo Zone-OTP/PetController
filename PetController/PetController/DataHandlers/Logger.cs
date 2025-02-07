ae5ea386b60b8bfc1d660a44db027075f8e087d2
Zone-OTP <zezva.mebagishvili5@mail.ru>
2025-02-07 13:52:08 +0400

    Changes to be committed:  	new file:   PetController/PetController/DataHandlers/Logger.cs

---
+++ PetController/PetController/DataHandlers/Logger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetController;
+
+namespace PetController
+{
+
+    public class PetEventArgs : EventArgs
+    {
+        public Pets Pet { get; }
+        public string Message { get; }
+        public LogLevel Level { get; }
+
+        public PetEventArgs(Pets pet, string message, LogLevel level = LogLevel.Info)
+        {
+            Pet = pet;
+            Message = message;
+            Level = level;
+        }
+    }
+    public class EventLogger
+    {
+        public EventLogger(PetManager petManager)
+        {
+            petManager.PetEvent += LogEvent;
+        }
+
+        private void LogEvent(object sender, PetEventArgs e)
+        {
+            string logLevelString = e.Level.ToString().ToUpper();
+            string logEntry = $"[{logLevelString}] {DateTime.Now} - {e.Message}";
+            Console.WriteLine(logEntry);
+            File.AppendAllText("log.txt", logEntry);
+        }
+    }
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error,
+        Debug
+    }
+
+
+}
